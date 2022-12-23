2ed253271467c325af6790bfd021d96d431b2e0a
Denis <apricot103@mail.ru>
2022-12-23 16:16:44 +0300

    delete test ui

--- ui/src/model/Organization.ts
+++
@@ -1,4 +0,0 @@
-export interface Organization {
-    id: number;
-    name: string;
-}
--- ui/src/pages/Users/Users.tsx
+++ ui/src/pages/Users/Users.tsx
@@ -1,24 +1,7 @@
-import OrganizationService from "../../service/OrganizationService";
-import {useEffect, useState} from "react";
-import {Organization} from "../../model/Organization";
-
 const Users = () => {
-  {/*Удалить*/}
-  const [organization, setUsers] = useState<Organization>();
-  {/*Удалить*/}
-  useEffect(() => {
-      OrganizationService.saveOrganization()
-          .then(response => setUsers(response));
-  }, []);
-
   return (
       <>
         <div>users</div>
-          {/*Удалить*/}
-          <div>
-              <span>{organization?.id}</span>
-              <span>{organization?.name}</span>
-          </div>
       </>
   );
 };
