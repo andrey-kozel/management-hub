9258368741341fcbd6a33c4a086f1abe4b3d0a2f
Denis <82713166+DenisSidorenkoQ@users.noreply.github.com>
2022-12-31 17:14:05 +0300

    Update OrganizationController.java

--- user-service/src/main/java/com/example/user/controller/OrganizationController.java
+++ user-service/src/main/java/com/example/user/controller/OrganizationController.java
@@ -6,7 +6,10 @@ import com.example.user.dto.OrganizationResponse;
 import com.example.user.facade.OrganizationFacade;
 import com.example.user.model.Organization;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/organizations")
