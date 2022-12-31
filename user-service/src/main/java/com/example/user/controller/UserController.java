75ce1ce785b417d77ff854a52bd46d54626fc71d
Denis <82713166+DenisSidorenkoQ@users.noreply.github.com>
2022-12-31 17:07:05 +0300

    Update UserController.java

--- user-service/src/main/java/com/example/user/controller/UserController.java
+++ user-service/src/main/java/com/example/user/controller/UserController.java
@@ -5,7 +5,6 @@ import com.example.user.dto.SaveOrGetUserRequest;
 import com.example.user.dto.UserResponse;
 import com.example.user.facade.UserFacade;
 import com.example.user.model.User;
-import com.example.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/v1/users")
