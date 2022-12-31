d348599384ce47d57e1df3f2d2a44b19f55201d5
Denis <82713166+DenisSidorenkoQ@users.noreply.github.com>
2022-12-31 17:06:24 +0300

    Update OrganizationClient.java

--- api-gateway/src/main/java/com/example/apigateway/client/OrganizationClient.java
+++ api-gateway/src/main/java/com/example/apigateway/client/OrganizationClient.java
@@ -4,7 +4,6 @@ import com.example.apigateway.dto.CreateOrganizationDto;
 import com.example.apigateway.dto.OrganizationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "organization", url = "${services.user.url}/api/v1/organizations")
 public interface OrganizationClient {
