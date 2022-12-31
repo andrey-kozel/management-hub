36dee164f000b4f4484573ee4b1512f4b0547716
Denis <82713166+DenisSidorenkoQ@users.noreply.github.com>
2022-12-31 17:07:42 +0300

    Update User.java

--- user-service/src/main/java/com/example/user/model/User.java
+++ user-service/src/main/java/com/example/user/model/User.java
@@ -2,7 +2,6 @@ package com.example.user.model;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Value;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
