0b5d7e396af621c310ef09e05b2a30acb97534bc
Andrey Kozel <andrey.kozel@spanning.com>
2023-01-06 23:31:49 +0300

    A small updates

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -5,19 +5,12 @@
 import create from 'zustand';
 import { User } from './model/SessionState';
 import SessionService from './service/SessionService';
-import SettingsService from "./service/SettingsService";
+import SettingsService from './service/SettingsService';
 
 export interface SessionState {
   user: User | null;
   loading: boolean;
   getSession: () => void;
-}
-
-export interface SettingsState {
-    accessToken: string;
-    loading: boolean;
-
-    getAccessToken: () => void;
 }
 
 export const useSessionStore = create<SessionState>(set => ({
@@ -35,18 +28,53 @@ export const useSessionStore = create<SessionState>(set => ({
   }
 }));
 
-export const useSettingsStore = create<SettingsState>(set => ({
-        accessToken: '',
-        loading: true,
-        getAccessToken: async () => {
-            try {
-                const accessToken = await SettingsService.getAccessToken(1);
-                set({accessToken});
-            } catch (error) {
-                set({accessToken: ''})
-            } finally {
-                set({loading: false})
-            }
-        }
+export interface SettingsState {
+  accessToken: string;
+  loading: boolean;
+  notification: {
+    message: string;
+    open: boolean;
+    duration: number;
+  };
+
+  getAccessToken: () => void;
+  setAccessToken: (token: string) => void;
+  saveToken: () => void;
+}
+
+export const useSettingsStore = create<SettingsState>((set, get) => ({
+    accessToken: '',
+    loading: true,
+    notification: {
+      open: false,
+      duration: 3000,
+      message: ''
+    },
+
+    getAccessToken: async () => {
+      try {
+        set({ loading: true });
+        const accessToken = await SettingsService.getAccessToken(1);
+        set({ accessToken, notification: { message: 'Token retrieved', duration: 3000, open: true } });
+      } catch (error) {
+        set({ accessToken: '', notification: { message: 'Failed to load token', duration: 3000, open: true } });
+      } finally {
+        set({ loading: false });
+      }
+    },
+
+    setAccessToken: (token: string) => {
+      set({ accessToken: token });
+    },
+
+    saveToken: async () => {
+      try {
+        const { accessToken } = get();
+        const token = await SettingsService.saveAccessToken(1, accessToken);
+        set({ accessToken: token, notification: { message: 'Token saved', duration: 3000, open: true } });
+      } catch (e) {
+        set({ accessToken: '', notification: { message: 'Failed to save token', duration: 3000, open: true } });
+      }
     }
-))
+  }
+));
