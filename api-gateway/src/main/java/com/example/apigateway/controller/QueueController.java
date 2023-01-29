5eaf5b86eb4205f6305807413038e35818db750e
Andrey Kozel <andrey.kozel@spanning.com>
2023-01-29 20:56:46 +0300

    Fixed checkstyle issue

--- api-gateway/src/main/java/com/example/apigateway/controller/QueueController.java
+++ api-gateway/src/main/java/com/example/apigateway/controller/QueueController.java
@@ -5,8 +5,10 @@ import com.example.apigateway.service.QueueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/queue")
 @RestController
