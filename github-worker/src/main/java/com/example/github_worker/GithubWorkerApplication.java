e987b6e7071c15eb815444ed41216a9c45f4ab2c
Denis <apricot103@mail.ru>
2023-01-30 22:48:47 +0300

    Added github worker

---
+++ github-worker/src/main/java/com/example/github_worker/GithubWorkerApplication.java
@@ -0,0 +1,11 @@
+package com.example.github_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubWorkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GithubWorkerApplication.class, args);
+    }
+}
