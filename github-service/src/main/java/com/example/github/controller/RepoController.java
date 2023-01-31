f2a4c8639cec5a659202672e0982a78e0a4bbaa0
Denis <apricot103@mail.ru>
2023-01-31 14:56:44 +0300

    fix repoController

--- github-service/src/main/java/com/example/github/controller/RepoController.java
+++ github-service/src/main/java/com/example/github/controller/RepoController.java
@@ -7,10 +7,7 @@ import com.example.github.dto.RepoDto;
 import com.example.github.model.Repo;
 import com.example.github.service.GithubService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1")
 @RestController
@@ -20,7 +17,7 @@ public class RepoController {
     private final RepoConverter repoConverter;
 
     @GetMapping("/organizations/{organizationId}/repositories")
-    protected List<RepoDto> getRepositories(@ModelAttribute("token") long organizationId) {
+    protected List<RepoDto> getRepositories(@PathVariable long organizationId) {
         List<Repo> listRepo = githubService.findAllRepositoryByToken(organizationId);
         return repoConverter.toDto(listRepo);
     }
