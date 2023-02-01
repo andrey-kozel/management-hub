17b249c6c25db8d92e75be1b0bdb3ab4a92b8cf6
Denis <apricot103@mail.ru>
2023-02-01 18:53:45 +0300

    Fix checkstyle

--- user-service/src/main/java/com/example/user/controller/OrganizationController.java
+++ user-service/src/main/java/com/example/user/controller/OrganizationController.java
@@ -8,7 +8,11 @@ import com.example.user.model.Organization;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/organizations")
