b35b5f70765e98dcfa242c226a56dc789e31cfb2
Denis <apricot103@mail.ru>
2023-02-02 15:36:36 +0300

    Rename organizationClient

--- github-worker/src/main/java/com/example/github_worker/client/GithubServiceClient.java
+++ github-worker/src/main/java/com/example/github_worker/client/GithubServiceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "github-service", url = "${services.github-service.url}/api/v1")
-public interface OrganizationClient {
+public interface GithubServiceClient {
     @PostMapping("/organizations/{organizationId}/repositories")
     void saveOrUpdateRepositories(@PathVariable("organizationId") Long organizationId, @RequestBody List<GithubRepositoryDto> repositoryList);
 }
